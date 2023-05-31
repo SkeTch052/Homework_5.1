13e5c6a8f7d62741cfa679249a93b532738a64b6
SkeTch <sketch13rus@mail.ru>
2023-05-31 17:56:13 +0300

    Without constructions

--- Homework_5.1.cpp
+++ Homework_5.1.cpp
@@ -1,20 +1,49 @@
-﻿// Homework_5.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
 
-#include <iostream>
+class Figure {
+protected:
+    int sides_count = 0;
+    std::string name = "Фигура";
+public:
+    int get_sides_count() {
+        return sides_count;
+    }
+    std::string get_name() {
+        return name;
+    }
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Triangle : public Figure {
+public:
+    void set () {
+        name = "Треугольник";
+        sides_count = 3;
+    }
+};
+
+class Quadrangle : public Figure {
+public:
+    void set () {
+        name = "Четырёхугольник";
+        sides_count = 4;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Figure figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон:\n";
+    std::cout << figure.get_name() << ": " << figure.get_sides_count() << std::endl;
+    triangle.set();
+    std::cout << triangle.get_name() << ": " << triangle.get_sides_count() << std::endl;
+    quadrangle.set();
+    std::cout << quadrangle.get_name() << ": " << quadrangle.get_sides_count() << std::endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
